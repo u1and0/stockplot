ae00f3593aefff53cd67b5f7e16692a2849b16e4
U1and0 <e01.ando60@gmail.com>
2017-03-14 17:31:19 -0400

    before matplotlib.dates format

--- common/candlechart.py
+++ common/candlechart.py
@@ -1,41 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.finance as fin
+from matplotlib import ticker
 import pandas as pd
-
-
-
-def mydate(x,pos):
-    try:
-        return xdate[int(x)]
-    except IndexError:
-        return ''
 
 
 def candlechart(ohlc, width=0.8):
     """入力されたデータフレームに対してローソク足チャートを返す
         引数:
-            * ohlc: 
+            * ohlc:
                 *データフレーム
                 * 列名に'open'", 'close', 'low', 'high'を入れること
                 * 順不同"
-            * widrh: ローソクの線幅 
+            * widrh: ローソクの線幅
         戻り値: ax: subplot"""
     fig, ax = plt.subplots()
     # ローソク足
     fin.candlestick2_ohlc(ax, opens=ohlc.open.values, closes=ohlc.close.values,
-                         lows=ohlc.low.values, highs=ohlc.high.values,
-                         width=width, colorup='r', colordown='b')
-    
+                          lows=ohlc.low.values, highs=ohlc.high.values,
+                          width=width, colorup='r', colordown='b')
+
     # x軸を時間にする
-    xdate = dfl.index
+    xdate = ohlc.index
     ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
+
+    def mydate(x, pos):
+        try:
+            return xdate[int(x)]
+        except IndexError:
+            return ''
+
     ax.xaxis.set_major_formatter(ticker.FuncFormatter(mydate))
 
     fig.autofmt_xdate()
     fig.tight_layout()
 
     return fig, ax
+
 
 def randomwalk(periods, start=pd.datetime.today().date(), index=None, name=None, tick=1, freq='B'):
     """periods日分だけランダムウォークを返す"""
