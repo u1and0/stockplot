127b374aaddd70e1b7b9eb92d34fbc5218057219
U1and0 <e01.ando60@gmail.com>
2017-03-20 12:31:21 -0400

    sma5 added df

--- common/basechart.py
+++ common/basechart.py
@@ -15,31 +15,39 @@ class base:
 
     def __init__(self, df):
         self.df = df
-
+        self.add_line = []
         # self.dt = dt
         self.fig = self.fig()
-        # self.add_line =
 
-    # def to_unix_time(self):
-    #     """datetimeをunix秒に変換
-    #     引数: datetime(複数指定可能)
-    #     戻り値: unix秒に直されたリスト"""
-    #     epoch =  datetime.utcfromtimestamp(0)
-    #     return [(i - epoch).total_seconds() * 1000 for i in self.dt]
+    # data make
 
+    # resampleは中でやったほうがいいのか外でやったほうがいいのか
+    # def resamp(self, ashi):
+    #     return df.ix[:, :4].resample(ashi).agg({'open':'first',
+    #                                          'high':'max', 'low':'min', 'close':'last'}).dropna()
+
+    def sma(self, window, columns='close'):
+        # adding = pyg.Scatter(x=self.df.index, y=ro, name='SMA5', line=pyg.Line(color='r'))
+        # self.add_line.extend(list(adding))
+        self.df['sma%d'% window] = self.df[columns].rolling(window).mean()
+        return self.df
+
+    # plot
     def fig(self):
         return FF.create_candlestick(self.df.open, self.df.high,
                                      self.df.low, self.df.close, dates=self.df.index)
 
-    def sma(self, window, columns='close'):
-        return self.data[columns].rolling(window).mean()
-
-    def plot(self, figname='candlestick_and_trace'):
-        pyo.plot(self.fig, filename=figname, validate=False)
-
+    def plot(self, filename='candlestick_and_trace.html'):
+        self.fig['data'].extend(self.add_line)
+        self.fig['layout'].update(xaxis={'showgrid': True})
+        pyo.plot(self.fig, filename=filename, validate=False)
 
 if __name__ == '__main__':
     np.random.seed(1)
     df = randomwalk(60 * 24 * 90, freq='T', tick=0.01,
                     start=pd.datetime(2017, 3, 20)).resample('B').ohlc() + 115
-    base(df).plot()
+    x = base(df)  # ohlcをbaseに渡す
+    k = x.sma(5)
+    print(k.head(5))
+
+    # x.plot()
