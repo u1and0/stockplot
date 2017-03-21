7a65abab08431a487345250db781534bcf99daa4
U1and0 <e01.ando60@gmail.com>
2017-03-21 05:22:34 -0400

    basechart v0.0.0

--- common/basechart.py
+++ common/basechart.py
@@ -15,39 +15,42 @@ class base:
 
     def __init__(self, df):
         self.df = df
-        self.add_line = []
-        # self.dt = dt
-        self.fig = self.fig()
+        self.add_line = []  # self.indicator()
 
-    # data make
-
-    # resampleは中でやったほうがいいのか外でやったほうがいいのか
-    # def resamp(self, ashi):
-    #     return df.ix[:, :4].resample(ashi).agg({'open':'first',
-    #                                          'high':'max', 'low':'min', 'close':'last'}).dropna()
-
+    # ----------DATA MAKE----------
     def sma(self, window, columns='close'):
-        # adding = pyg.Scatter(x=self.df.index, y=ro, name='SMA5', line=pyg.Line(color='r'))
-        # self.add_line.extend(list(adding))
-        self.df['sma%d'% window] = self.df[columns].rolling(window).mean()
+        """Simple Moving Average
+        windowの足の分だけ移動平均
+        dfに格納する
+        column名はSMA{移動した足}
+
+        引数: 
+            window: 移動足
+            columns: 平均を適用する足{open, high, low, close}どれか
+        戻り値: smaを格納したdf"""
+        colname = 'sma%d' % window
+        self.df[colname] = self.df[columns].rolling(window).mean()
+        plotter = pyg.Scatter(x=self.df.index, y=self.df[colname],
+                              name='SMA%d' % window, line=pyg.Line())
+        self.add_line.append(plotter)
         return self.df
 
-    # plot
-    def fig(self):
-        return FF.create_candlestick(self.df.open, self.df.high,
-                                     self.df.low, self.df.close, dates=self.df.index)
-
+    # ---------PLOT----------
     def plot(self, filename='candlestick_and_trace.html'):
-        self.fig['data'].extend(self.add_line)
-        self.fig['layout'].update(xaxis={'showgrid': True})
-        pyo.plot(self.fig, filename=filename, validate=False)
+        fig = FF.create_candlestick(self.df.open, self.df.high,
+                                    self.df.low, self.df.close, dates=self.df.index)
+        fig['data'].extend(self.add_line)
+        fig['layout'].update(xaxis={'showgrid': True})
+        pyo.plot(fig, filename=filename, validate=False)
+
 
 if __name__ == '__main__':
     np.random.seed(1)
     df = randomwalk(60 * 24 * 90, freq='T', tick=0.01,
                     start=pd.datetime(2017, 3, 20)).resample('B').ohlc() + 115
     x = base(df)  # ohlcをbaseに渡す
-    k = x.sma(5)
-    print(k.head(5))
-
-    # x.plot()
+    x.sma(5)
+    x.sma(25)
+    x.sma(35)
+    print(x.df.head(5))
+    x.plot()
