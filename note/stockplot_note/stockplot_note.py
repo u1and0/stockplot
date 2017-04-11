81445f5b115b791dc8443b57282e760cf23e01d9
U1and0 <e01.ando60@gmail.com>
2017-04-11 15:25:46 -0400

    edit start end

--- note/stockplot_note/stockplot_note.py
+++ note/stockplot_note/stockplot_note.py
@@ -3,6 +3,128 @@
 
 # [仮タイトル]plotlyでキャンドルチャートプロット
 
+# # 下準備
+
+# ## モジュールインポート
+
+# In[1]:
+
+import sys, os
+sys.path.append('../../common/')
+
+
+# ## サンプルデータの作成
+
+# In[2]:
+
+np.random.seed(9)
+from randomwalk import randomwalk
+df = randomwalk(60 * 24 * 90, freq='T', tick=0.01, start=pd.datetime(2017, 3, 20)
+                ).resample('B').ohlc() + 115  # 90日分の1分足を日足に直す
+
+
+# ランダムウォークの作成。
+# 詳しくは[pythonでローソク足(candle chart)の描画](http://qiita.com/u1and0/items/1d9afdb7216c3d2320ef)をご覧ください。
+
+# ## pd.DataFrame型をStockDataFrame型に変換
+
+# In[ ]:
+
+from stockstats import StockDataFrame
+sdf = StockDataFrame(df.copy())
+
+
+# # StockPlotクラスの使用方法
+
+# ## stockplot.pyのインポート
+
+# In[ ]:
+
+from stockplot import StockPlot
+
+
+# In[ ]:
+
+# StockPlotクラスのインスタンス化
+x = StockPlot(sdf)
+
+
+# In[ ]:
+
+# キャンドルチャートのプロット
+x.candle_plot()
+
+
+# # 指標
+
+# ## 指標の追加
+
+# In[ ]:
+
+# 終値25日移動平均線追加
+x.add_indicator('close_25_sma')
+
+
+# `add_indicator`関数の戻り値は`StockDataFrame`クラスで`get`メソッドを使った時と同じ結果が返ってきます。
+# 
+# 裏でplotly形式に直され、インスタンス変数self.figに追加されます。
+
+# In[ ]:
+
+# キャンドルチャートと追加された指標のプロット
+x.candle_plot()
+
+
+# 終値25日移動平均線が追加されました。
+
+# In[ ]:
+
+# 終値25日指数移動平均線の追加とプロット
+x.add_indicator('close_25_ema')
+x.candle_plot()
+
+
+# 先ほど追加された終値25日移動平均線に加え、終値25日指数移動平均線が追加されました。
+
+# ## 指標の削除
+
+# In[ ]:
+
+y = StockPlot(sdf)
+# 10,11,12,13足移動平均線
+for i in range(10, 14):
+    y.add_indicator('close_{}_sma'.format(i))
+y.candle_plot()
+
+
+# In[ ]:
+
+# 10, 12足移動平均線の削除
+for i in (10, 12):
+    y.remove_indicator('close_{}_sma'.format(i))
+y.candle_plot()
+
+
+# # ファイルへのエクスポート
+
+# StockPlotクラスはPlotlyのhtmlエクスポートする`plotly.offline.iplot`(Jupyter Notebook形式)で図を吐き出します。
+# つまり、ファイルへのエクスポートがされません。
+# 
+# ファイルとして吐き出したいときは以下のメソッドを使います。
+# 
+# * html形式: `plotly.offline.plot()`
+# * png, svg, jpeg, webp形式: `plotly.offline.image`
+
+# In[ ]:
+
+pyo.plot(y.fig, filename='candle_y.html', validate=False)
+
+
+# In[ ]:
+
+pyo.plot(y.fig, image='png', image_filename='candle_y')
+
+
 # # パッケージ紹介
 
 # ## Plotly
@@ -29,7 +151,7 @@
 # 
 # `stockstats`は金融指標を簡単に取得できる改造pandas.DataFrameクラスです。
 
-# In[1]:
+# In[ ]:
 
 # 使い方
 np.random.seed(2)
@@ -41,14 +163,14 @@ sdf
 
 # 見た目に変化はありませんが金融指標を`stockstats`の文法に従って`get`メソッド、またはディクショナリの取得をすると、金融指標のカラムが追加されます。
 
-# In[2]:
+# In[ ]:
 
 sdf.get('close_5_sma'); sdf
 
 
 # close_5_sma: 終値の5足移動平均線が追加されました。
 
-# In[3]:
+# In[ ]:
 
 sdf['close_5_sma']; sdf
 
@@ -64,49 +186,20 @@ sdf['close_5_sma']; sdf
 
 # ## stockplot
 
-# これが私が作ったやつです。
+# これが今、紹介した私が作ったやつです。
 # stockstatsを楽にplotするためのクラスStockPlotを作成しました。
-# この記事はこれの使い方紹介です。
-
-# # 下準備
-
-# ## モジュールインポート
-
-# In[4]:
-
-import sys, os
-sys.path.append('../../common/')
-
-
-# ## サンプルデータの作成
-
-# In[5]:
-
-np.random.seed(9)
-from randomwalk import randomwalk
-df = randomwalk(60 * 24 * 90, freq='T', tick=0.01, start=pd.datetime(2017, 3, 20)
-                ).resample('B').ohlc() + 115  # 90日分の1分足を日足に直す
-
-
-# ランダムウォークの作成。
-# 詳しくは[pythonでローソク足(candle chart)の描画](http://qiita.com/u1and0/items/1d9afdb7216c3d2320ef)をご覧ください。
-
-# ## pd.DataFrame型をStockDataFrame型に変換
-
-# In[6]:
-
-from stockstats import StockDataFrame
-sdf = StockDataFrame(df.copy())
-
-
-# # StockPlotクラスの使用方法
-
-# ## stockplot.pyのインポート
-
-# In[7]:
-
-import stockplot as sp
-
+# 
+# プログラミンg府初心者故クラスの引継ぎとかよくわかんなくてこんな感じになりました。
+# 
+# できたら
+# 
+# ```python
+# df = randomwalk(60 * 24 * 90, freq='T', tick=0.01, start=pd.datetime(2017, 3, 20)
+#                 ).resample('B').ohlc() + 115  # 90日分の1分足を日足に直す
+# sdf = StockDataFrame(df)
+# x = StockPlot(sdf)
+# x.candle_plot()
+# ```
 
 # stockplot.pyのソースコード
 
@@ -208,91 +301,4 @@ class StockPlot:
             if dicc['name'] == INDI:
                 self.fig['data'].remove(dicc)
                 return dicc
-
-
-# In[8]:
-
-# StockPlotクラスのインスタンス化
-x = sp.StockPlot(sdf)
-
-
-# In[9]:
-
-# キャンドルチャートのプロット
-x.candle_plot()
-
-
-# # 指標
-
-# ## 指標の追加
-
-# In[10]:
-
-# 終値25日移動平均線追加
-x.add_indicator('close_25_sma')
-
-
-# `add_indicator`関数の戻り値は`StockDataFrame`クラスで`get`メソッドを使った時と同じ結果が返ってきます。
-# 
-# 裏でplotly形式に直され、インスタンス変数self.figに追加されます。
-
-# In[11]:
-
-# キャンドルチャートと追加された指標のプロット
-x.candle_plot()
-
-
-# 終値25日移動平均線が追加されました。
-
-# In[12]:
-
-# 終値25日指数移動平均線の追加とプロット
-x.add_indicator('close_25_ema')
-x.candle_plot()
-
-
-# 先ほど追加された終値25日移動平均線に加え、終値25日指数移動平均線が追加されました。
-
-# ## 指標の削除
-
-# In[13]:
-
-y = StockPlot(sdf)
-# 10,11,12,13足移動平均線
-for i in range(10, 14):
-    y.add_indicator('close_{}_sma'.format(i))
-y.candle_plot()
-
-
-# In[ ]:
-
-# 10, 12足移動平均線の削除
-for i in (10, 12):
-    y.remove_indicator('close_{}_sma'.format(i))
-y.candle_plot()
-
-
-# # ファイルへのエクスポート
-
-# StockPlotクラスはPlotlyのhtmlエクスポートする`plotly.offline.iplot`(Jupyter Notebook形式)で図を吐き出します。
-# つまり、ファイルへのエクスポートがされません。
-# 
-# ファイルとして吐き出したいときは以下のメソッドを使います。
-# 
-# * html形式: `plotly.offline.plot()`
-# * png, svg, jpeg, webp形式: `plotly.offline.image`
-
-# In[ ]:
-
-pyo.plot(y.fig, filename='candle_y.html', validate=False)
-
-
-# In[ ]:
-
-pyo.plot(y.fig, image='png', image_filename='candle_y')
-
-
-# In[ ]:
-
-
 
