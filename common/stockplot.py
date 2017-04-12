617bfd05f65e9ff64a218d20e7bf7dd26f93592d
U1and0 <e01.ando60@gmail.com>
2017-04-12 00:12:12 -0400

    qiita export

--- common/stockplot.py
+++ common/stockplot.py
@@ -16,19 +16,21 @@ class StockPlot:
     # なにがしたい
 
     * StockDataFrameにプロット能力を持たせたい。
-    * プロット能力はStockDataFrameクラスにメソッドを付与してあげる。
+    * StockPlotの属性
+        * StockDataFrame: 金融指標を取得しやすい改造pandas.DataFrame
+        * _fig: plotlyのプロットデータ        
 
     ```python
     df = randomwalk(60 * 24 * 90, freq='T', tick=0.01,
                     start=pd.datetime(2017, 3, 20)).resample('B').ohlc() + 115  # 90日分の1分足を日足に直す
     dfs = stockstats.StockDataFrame(df)
-    dfs.add_indicator('hoge'): インジケーターの追加
+    dfs.add('hoge'): インジケーターの追加
     dfs.candle_plot(): キャンドルチャートとインジケータの表示
-    dfs.remove_indicator('hoge'): インジケーターの削除
+    dfs.remove('hoge'): インジケーターの削除
     ```
 
     # メソッド詳細
-    * dfs.add_indicator('hoge')
+    * dfs.add('hoge')
         * dfs.get('hoge')を実行して、グラフに挿入するデータフレームを入手する
             > `indi = dfs.get('hoge')`
         * プロットするための形plotterに変換してやる
@@ -38,33 +40,26 @@ class StockPlot:
 
     * dfs.candle_plot()
         * `fig = FF.create_candlestick... `でキャンドルチャートを取得できる
-        * figに対してadd_indicator / remove_inidcatorで指標の追加 / 削除が行われる。
+        * figに対してadd / remove_inidcatorで指標の追加 / 削除が行われる。
 
     * dfs.plot()
         * plt.show()に当たるのかな
 
         ```python
-                self.fig['layout'].update(xaxis={'showgrid': True})  # figのレイアウト調整をして
-                pyo.plot(self.fig, filename=filename, validate=False)  # plotlyでhtmlとしてプロットする
+                self._fig['layout'].update(xaxis={'showgrid': True})  # figのレイアウト調整をして
+                pyo.plot(self._fig, filename=filename, validate=False)  # plotlyでhtmlとしてプロットする
         ```
 
-        別にdataframe的な操作は必要ないから、
-        StockDataFrameのサブクラスになる必要はないので
-
-        強いていうなら、StockDataFrameにfigという属性持たせて、
-        plotlyとつなげたいから
-        def __init()__をStockDataFrameにあてがってあげればいいのか
-        StockDataFrame.__init__ = __init__
-
     # TODO
-    * koma_ashi
+    * heikin_plot
+    * pop
     * subplot
 
     """
 
-    def __init__(self, sdf):
+    def __init__(self, sdf: ss.StockDataFrame):
         self.StockDataFrame = sdf
-        self.fig = FF.create_candlestick(self.StockDataFrame.open,
+        self._fig = FF.create_candlestick(self.StockDataFrame.open,
                                          self.StockDataFrame.high,
                                          self.StockDataFrame.low,
                                          self.StockDataFrame.close,
@@ -74,25 +69,25 @@ class StockPlot:
         """StockDataFrameをキャンドルチャート化する
         引数: dfs: StockDataFrame
         戻り値: plotly plot"""
-        self.fig['layout'].update(xaxis={'showgrid': True})
-        ax = pyo.iplot(self.fig, filename=filebasename + '.html', validate=False)
-        # pyo.plot(self.fig, image='png', image_filename=filebasename, validate=False)
+        self._fig['layout'].update(xaxis={'showgrid': True})
+        ax = pyo.iplot(self._fig, filename=filebasename + '.html', validate=False)
+        # pyo.plot(self._fig, image='png', image_filename=filebasename, validate=False)
         return ax
 
-    def add_indicator(self, indicator):
+    def append(self, indicator):
         indi = self.StockDataFrame.get(indicator)
         plotter = go.Scatter(x=indi.index, y=indi,
                              name=indicator.upper().replace('_', ' '))  # グラフに追加する形式変換
-        self.fig['data'].append(plotter)
+        self._fig['data'].append(plotter)
         return indi
 
-    def remove_indicator(self, indicator):
+    def remove(self, indicator):
         indi = indicator.lower().replace(' ', '_')
         INDI = indicator.upper().replace('_', ' ')
         self.StockDataFrame.pop(indi)
-        for dicc in self.fig['data']:
+        for dicc in self._fig['data']:
             if dicc['name'] == INDI:
-                self.fig['data'].remove(dicc)
+                self._fig['data'].remove(dicc)
                 return dicc
 
 
@@ -110,11 +105,11 @@ if __name__ == '__main__':
 
     # # Add indicator
     for i in range(10, 14):
-        x.add_indicator('close_{}_sma'.format(i))
+        x.add('close_{}_sma'.format(i))
 
     # # Remove indicator
     for i in (10, 12):
-        x.remove_indicator('close_{}_sma'.format(i))
+        x.remove('close_{}_sma'.format(i))
 
     # # Plot Candle chart
     x.candle_plot()
