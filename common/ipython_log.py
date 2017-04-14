9c84e08194feaa39b5ccb83889eea5ec8a1064e4
U1and0 <e01.ando60@gmail.com>
2017-04-14 01:23:06 -0400

    ipylog.py

--- common/ipython_log.py
+++ common/ipython_log.py
@@ -1,11 +1,35 @@
 # IPython log file
-
+# ----------General Module----------
+import numpy as np
+import pandas as pd
+import stockstats as ss
+# ----------User Module----------
 from randomwalk import randomwalk
-df = randomwalk(10000, freq='T').resample('H').ohlc(); df
-# random OHLC made END
-import basechart
-x = basechart.Base(df)
-x.plot()
-x.add('close_5_sma')
-x.plot()
+from stockplot import StockPlot
+# ----------Plotly Module----------
+import plotly.offline as pyo
+pyo.init_notebook_mode(connected=True)
 
+# Make sample data
+np.random.seed(1)
+df = randomwalk(60 * 24 * 90, freq='T', tick=0.01, start=pd.datetime(2017, 3, 20)).resample('B').ohlc() + 115  # 90日分の1分足を日足に直す
+
+# Convert DataFrame as StockDataFrame
+sdf = ss.StockDataFrame(df.copy())
+
+# Convert StockDataFrame as StockPlot
+sp = StockPlot(sdf.copy())
+
+# # Add indicator
+# for i in range(10, 17):
+#     sp.append('close_{}_sma'.format(i))
+
+# # Remove indicator
+# for i in [13, 11]:
+#     sp.remove('close_{}_sma'.format(i))
+
+# # Pop indicator
+# sp.pop()
+
+# # Plot Candle chart
+# sp.candle_plot(how='html')
