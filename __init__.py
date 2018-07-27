025c158c9af5ebdf02cc899f25d66f74a8b3a137
u1and0 <e01.ando60@gmail.com>
2018-07-27 22:16:01 +0900

    [add] cross_currency(): Making cross currency data

--- __init__.py
+++ __init__.py
@@ -4,3 +4,4 @@ from .read_nikkei import read_nikkei
 from .stockplot import StockPlot
 from .randomwalk import randomwalk
 from .heatmap import heat_candle
+from .heatmap import cross_currency
--- heatmap.py
+++ heatmap.py
@@ -26,8 +26,82 @@ def heat_candle(df: pd.DataFrame) -> pd.Series:
     return sr
 
 
+def cross_currency(panel, base: str, freq: str):
+    """Convert 1 min panel to another base currency
     description:
+        Function for calculating cross USD from cross JPY
+        EURUSD = EURJPY / USDJPY so `base='USDJPY'`
 
+    args:
+        panel: cross currency panel XJPY
+        base: if dorrer base then 'USDJPY'
+        freq: resample frequency
 
     usage:
+        >>> pl = pd.read_pickle('~/Data/XJPY1m-2005-2018.pkl')
+        >>> cross_currency(panel=xjpy, base='USDJPY', freq='D')
+
+    # USD cross
+        USDUSD = USDJPY / USDJPY
+        EURUSD = EURJPY / USDJPY
+        JPYUSD =      1 / USDJPY
+        GBPUSD = GBPJPY / USDJPY
+        CADUSD = CADJPY / USDJPY
+        CHFUSD = CHFJPY / USDJPY
+        AUDUSD = AUDJPY / USDJPY
+    # EUR cross
+        USDEUR = USDJPY / EURJPY
+        EUREUR = EURJPY / EURJPY
+        JPYEUR =      1 / EURJPY
+        GBPEUR = GBPJPY / EURJPY
+        CADEUR = CADJPY / EURJPY
+        CHFEUR = CHFJPY / EURJPY
+        AUDEUR = AUDJPY / EURJPY
+    # GBP cross
+        USDGBP = USDJPY / GBPJPY
+        EURGBP = EURJPY / GBPJPY
+        JPYGBP =      1 / GBPJPY
+        GBPGBP = GBPJPY / GBPJPY
+        CADGBP = CADJPY / GBPJPY
+        CHFGBP = CHFJPY / GBPJPY
+        AUDGBP = AUDJPY / GBPJPY
+    # CAD cross
+        USDCAD = USDJPY / CADJPY
+        EURCAD = EURJPY / CADJPY
+        JPYCAD =      1 / CADJPY
+        GBPCAD = GBPJPY / CADJPY
+        CADCAD = CADJPY / CADJPY
+        CHFCAD = CHFJPY / CADJPY
+        AUDCAD = AUDJPY / CADJPY
+    # CHF cross
+        USDCHF = USDJPY / CHFJPY
+        EURCHF = EURJPY / CHFJPY
+        JPYCHF =      1 / CHFJPY
+        GBPCHF = GBPJPY / CHFJPY
+        CADCHF = CADJPY / CHFJPY
+        CHFCHF = CHFJPY / CHFJPY
+        AUDCHF = AUDJPY / CHFJPY
+    # AUD cross
+        USDAUD = USDJPY / AUDJPY
+        EURAUD = EURJPY / AUDJPY
+        JPYAUD =      1 / AUDJPY
+        GBPAUD = GBPJPY / AUDJPY
+        CADAUD = CADJPY / AUDJPY
+        CHFAUD = CHFJPY / AUDJPY
+        AUDAUD = AUDJPY / AUDJPY
     """
+    cross_panel = {}
+    for item in panel.items:
+        cross_series = panel[item, :, 'close'] / panel[base, :, 'close']
+        cross_dataframe = cross_series.resample(freq).ohlc().dropna()
+        new = item[:3] + base[:3]
+        cross_panel[new] = cross_dataframe
+    reverse = base[3:] + base[:3]
+    reverse_series = panel[base].rdiv(1)['close']
+    reverse_dataframe = reverse_series.resample(freq).ohlc().dropna()
+    cross_panel[reverse] = reverse_dataframe
+    return pd.Panel(cross_panel)
+
+
+# To use xjpy.cross_currency('USDJPY', 'D') <- based USD, resample daily
+setattr(pd.Panel, 'cross_currency', cross_currency)
